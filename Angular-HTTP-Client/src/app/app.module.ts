ab95ef77ec36255c84baaa5bb265914704d13698
Lemon <sabinbasi111@gmail.com>
2024-09-05 08:30:13 +0545

    Add http-client project

---
+++ Angular-HTTP-Client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
+import { TaskDetailsComponent } from './dashboard/task-details/task-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    CreateTaskComponent,
+    TaskDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
