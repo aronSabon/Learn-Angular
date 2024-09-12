5b30c222eb1c55eab67781cc517b83359d8ef5bc
Lemon <sabinbasi111@gmail.com>
2024-09-12 12:19:45 +0545

    multiple interceptors

--- Angular-HTTP-Client/src/app/app.module.ts
+++ Angular-HTTP-Client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TaskDetailsComponent } from './dashboard/task-details/task-details.comp
 import { FormsModule } from '@angular/forms';
 import {  HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptorService } from './Services/auth.interceptor.service';
+import { LoggingInterceptorService } from './Services/logging-interceptor-service';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { AuthInterceptorService } from './Services/auth.interceptor.service';
   ],
   providers: [
     { provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true},
+    { provide:HTTP_INTERCEPTORS,useClass:LoggingInterceptorService,multi:true},
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
