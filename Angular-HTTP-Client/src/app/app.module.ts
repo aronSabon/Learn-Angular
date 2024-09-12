1a9cabd90ce2672dc1b96311ab9ae7473f1cf8e5
Lemon <sabinbasi111@gmail.com>
2024-09-12 10:43:41 +0545

    inceptor

--- Angular-HTTP-Client/src/app/app.module.ts
+++ Angular-HTTP-Client/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { HeaderComponent } from './header/header.component';
 import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
 import { TaskDetailsComponent } from './dashboard/task-details/task-details.component';
 import { FormsModule } from '@angular/forms';
-import {  HttpClientModule } from '@angular/common/http';
+import {  HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptorService } from './Services/auth.interceptor.service';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import {  HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
+    { provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true},
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
