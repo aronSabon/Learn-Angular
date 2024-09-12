0d5cc764c62532b03de7616da25326ba227a53dc
Lemon <sabinbasi111@gmail.com>
2024-09-12 12:06:26 +0545

    modify request and response through interceptor

--- Angular-HTTP-Client/src/app/Services/auth.interceptor.service.ts
+++ Angular-HTTP-Client/src/app/Services/auth.interceptor.service.ts
@@ -1,10 +1,18 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const modifyReq= req.clone({headers: req.headers.append('auth','lemonauth')});
         console.log('auth Interceptor called')
-        return next.handle(req);
+        return next.handle(modifyReq).pipe(tap((event) => {
+            if(event.type===HttpEventType.Response ){
+                console.log('response event:')
+                console.log(event);
+                console.log("response arrived. Response:")
+                console.log(event.body);
+            }
+        }))
     }
 
 }
