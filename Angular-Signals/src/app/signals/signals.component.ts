f8b617eec9edfd684c267350bafadcd055a2446a
Lemon <sabinbasi111@gmail.com>
2024-09-16 12:34:11 +0545

    update mutable value(array) using update. mutate not working

--- Angular-Signals/src/app/signals/signals.component.ts
+++ Angular-Signals/src/app/signals/signals.component.ts
@@ -7,12 +7,22 @@ import { Component, signal } from '@angular/core';
 })
 export class SignalsComponent {
   counter=signal(0);
-  message:string[]=[];
+  message=signal<string[]>([])
   increment(){
-    this.counter.set(this.counter()+1);
+    // this.counter.set(this.counter()+1);
+    this.counter.update((prevValue) => prevValue+1)
+    this.message.update((prevMessage)=>[...prevMessage,'current value of counter is:' + this.counter()])
+    //mutate not working
+    // this.message.mutate((prevMessage)=> prevMessage.push('current value of counter is :' + this.counter()))
   }
   decrement(){
     this.counter.update((prevValue) => prevValue-1)
+    this.message.update( (prevMessage) =>{
+      const updatedMessage=[...prevMessage];
+      updatedMessage.pop();
+      return updatedMessage;
+    })
+
   }
 
 }
